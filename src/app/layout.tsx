ab246903df804676dca0fdc147f66909b40560d0
ChathuraLiyanapathirana <cbuddhika3@gmail.com>
2024-02-20 06:50:44 +0530

    favicon changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`portfolio-body ${inter.className}`}>
         {children}
         <SpeedInsights />
       </body>
